786951af66ba5cad1eeb8dfbf0fd4fa3a3b9bbed
ljsinsist20 <2593161116@qq.com>
2021-11-01 19:46:37 +0800

    9-增加会员列表展示接口

--- service-core/src/main/java/com/atguigu/srb/core/service/UserInfoService.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/UserInfoService.java
@@ -1,9 +1,12 @@
 package com.atguigu.srb.core.service;
 
 import com.atguigu.srb.core.pojo.entity.UserInfo;
+import com.atguigu.srb.core.pojo.query.UserInfoQuery;
 import com.atguigu.srb.core.pojo.vo.LoginVO;
 import com.atguigu.srb.core.pojo.vo.RegisterVO;
 import com.atguigu.srb.core.pojo.vo.UserInfoVO;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -18,4 +21,6 @@ public interface UserInfoService extends IService<UserInfo> {
     void register(RegisterVO registerVO);
 
     UserInfoVO login(LoginVO loginVO, String ip);
+
+    IPage<UserInfo> listPage(Page<UserInfo> pageParam, UserInfoQuery userInfoQuery);
 }
